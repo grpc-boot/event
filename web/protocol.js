650a6d7a2ab1f9c4be38351e265f0da24b09cba6
jianghaiqiang <jianghaiqiang@360.cn>
2022-11-09 10:06:08 +0800

    update protocol

--- web/protocol.js
+++ web/protocol.js
@@ -1,7 +1,8 @@
-function encrypt(msg, k, v) {
-    k = !k ? "SD#$523asz7*&^df" : k;
-    v = !v ? "312c45cDvd$!F~12" : v;
+const LevelJson = 0;
+const LevelV1   = 1;
+const LevelV2   = 2;
 
+function encrypt(msg, k, v) {
     return CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(msg),
         CryptoJS.enc.Utf8.parse(k), {
             iv: CryptoJS.enc.Utf8.parse(v)
@@ -10,9 +11,6 @@ function encrypt(msg, k, v) {
 }
 
 function decrypt(base64Data, k, v) {
-    k = !k ? "SD#$523asz7*&^df" : k;
-    v = !v ? "312c45cDvd$!F~12" : v;
-
     return CryptoJS.AES.decrypt(base64Data,
         CryptoJS.enc.Utf8.parse(k), {
             iv: CryptoJS.enc.Utf8.parse(v)
@@ -78,10 +76,58 @@ Package.prototype.unpack = function (data) {
     return true;
 }
 
-function WsProtocol(level, k, v) {
+function WsProtocol(uri, level, k, v) {
+    this.uri   = uri;
     this.level = level;
     this.k     = k;
     this.v     = v;
+    this.ws    = null;
+    this.protocol = null;
+}
+
+WsProtocol.prototype.buildUri = function () {
+    let url = this.uri;
+
+    if(url.indexOf('?') > 0) {
+        url += '&';
+    }else {
+        url += '?';
+    }
+
+    url+= ('l='+this.level);
+    let key = randKey();
+
+    switch (this.level) {
+        case LevelJson:
+            this.protocol = new Json();
+            break;
+        case LevelV1:
+            key += randKey();
+            this.protocol = new V1(key);
+            url+= ('&k='+encrypt(key, this.k, this.v).ciphertext.toString());
+            break;
+        case LevelV2:
+            this.protocol = new V2(key);
+            url+= ('&k='+encrypt(key, this.k, this.v).ciphertext.toString());
+            break;
+    }
+
+    return url;
+}
+
+WsProtocol.prototype.dial = function () {
+    let url = this.buildUri();
+    console.log(url);
+    try {
+        this.ws = new WebSocket(url);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+WsProtocol.prototype.accept = function () {
+
 }
 
 function Json() {
@@ -145,4 +191,45 @@ V1.prototype.unpack = function (data) {
 function V2(k) {
     this.k     = k;
     this.level = 2;
+}
+
+V2.prototype.setV = function(v) {
+    this.v = v;
+}
+
+V2.prototype.pack = function (pkg) {
+    let data = encrypt(pkg.pack(), this.k, this.v).toString();
+    let signVal = parseInt(CryptoJS.MD5(data).toString().substring(0, 8), 16);
+    let bytes = String.fromCharCode.apply(String, [1, 58])
+        + int2HexWithPad(pkg.id, 4)
+        + String.fromCharCode.apply(String, intToBytesBigEndian(signVal, 4))
+        + data;
+    return bytes;
+}
+
+V2.prototype.unpack = function (data) {
+    if (!data || data.length < 11 || data[0] !== String.fromCharCode(1) || data[1] !== ':'){
+        return false;
+    }
+
+    let id = parseInt(data.substring(2, 6), 16);
+    if(id < 1 || id > 0xffff) {
+        return false;
+    }
+
+    let signVal = parseInt(CryptoJS.MD5(data.substring(10)).toString().substring(0, 8), 16);
+    if(data.substring(6, 10) !== String.fromCharCode.apply(String, intToBytesBigEndian(signVal, 4))) {
+        return false;
+    }
+
+    let res = decrypt(data.substring(10), this.k, this.v);
+    let dataStr = res.toString(CryptoJS.enc.Utf8);
+    let pkg = new Package();
+    if(pkg.unpack(dataStr)) {
+        if(pkg.id !== id) {
+            return false;
+        }
+        return pkg;
+    }
+    return false;
 }
